9fa02968e1adbb1a36002c2aa901b721c0a70629
aliprogrammer309 <aliprogrammer309@gmail.com>
2022-11-05 08:13:32 +0500

    first commit

---
+++ src/api.js
@@ -0,0 +1,14 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'b01ab4de89mshde919e6549c1185p12c446jsnbc057abf1d85',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "56c3160a566b876b952ff0a0dc940d45";
+
+
